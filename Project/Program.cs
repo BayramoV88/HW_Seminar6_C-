096ccdfeac38a10a5b6e4978e881369666770b91
Vadim <bayramovva@gmail.com>
2022-08-12 17:05:06 +0300

    Add Zadacha43

--- Project/Program.cs
+++ Project/Program.cs
@@ -21,7 +21,28 @@
             }
             Console.WriteLine($"Количество элементов больше нуля равно {count}");
         }
-    
+
+        void Zadacha43()
+        {
+            Console.WriteLine("Введите значение b1");
+            double b1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение k1");
+            double k1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение b2");
+            double b2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение k2");
+            double k2 = Convert.ToInt32(Console.ReadLine());
+
+            double X = 0;
+            double Y = 0;
+            if (k1 == k2) Console.WriteLine("прямые не пересекаются - параллельны");
+            else
+            {
+                X = Math.Round(((b2-b1)/(k1-k2)), 1);
+                Y = Math.Round((X*k2 + b2), 1);
+                Console.WriteLine($"точка пересечения прямых находиться по координатам {(X, Y)}");
+            }
+        }
 
         void FillArray(int[] array, int startNum = -100, int finishNum = 100)
         {
@@ -40,11 +61,9 @@
             }
             Console.WriteLine();
         }
-
-        Zadacha41();
-
-
-
-    }
         
+        
+        //Zadacha41();
+        Zadacha43();
+    }
 }
